19a6b5712c090dff7294f84f11e05a20cedc3905
Satyam Rathod <satyamrathod57@gmail.com>
2026-01-31 18:21:49 +0530

    adding the new upgrades to the python expense tracker

--- expense_tracker.py
+++ expense_tracker.py
@@ -74,14 +74,6 @@ def delete_expense(expenses):
    except ValueError:
        print("Enter a valid input\n")
 
-def monthly_total(expenses):
-   month = input("Enter month(YYYY-MM): ")
-   total = 0.0
-   for exp in expenses:
-       if exp["date"].startswith(month):
-           total = total + exp["amount"]
-   print(f"total expense of {month}: ${total}")
-
 def main():
    expenses = load_expenses()
 
